6e324c62786be55af0fd072eba4c995eebe70487
Aylon Carrijo <aylon.gabriel@outlook.com>
2023-03-21 17:10:01 -0300

    build: add rollup configuration

---
+++ scripts/rollup.config.js
@@ -0,0 +1,32 @@
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import { defineConfig } from "rollup";
+import del from "rollup-plugin-delete";
+import external from "rollup-plugin-peer-deps-external";
+import typescript from "rollup-plugin-ts";
+
+export const createRollupConfig = (
+  /** @type {import("rollup").RollupOptions} */ config
+) =>
+  defineConfig(({ watch }) => ({
+    input: "./src/index.ts",
+    output: [
+      {
+        file: "./build/cjs/index.cjs",
+        format: "cjs",
+        interop: "auto",
+      },
+      {
+        file: "./build/esm/index.mjs",
+        format: "esm",
+      },
+    ],
+    plugins: [
+      !watch && del({ targets: "build/*" }),
+      external({ includeDependencies: true }),
+      resolve(),
+      commonjs(),
+      typescript(),
+    ],
+    ...config,
+  }));
