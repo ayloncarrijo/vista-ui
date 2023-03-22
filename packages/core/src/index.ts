4a4f3a8b641b7b307177c911653820d833f7c8a5
Aylon Carrijo <aylon.gabriel@outlook.com>
2023-03-21 21:57:44 -0300

    chore(react): add storybook configuration

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,7 @@
 export * from "./stitches.config";
+export * from "./stitches/global";
+export * from "./stitches/keyframes";
+export * from "./types/stitches";
+export * from "./types/typography";
+export * from "./utils/color-scheme";
+export * from "./utils/typography";
